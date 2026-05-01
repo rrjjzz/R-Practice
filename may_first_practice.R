0ac71df25f51302bd07f13d95b47b1ded8bd3cc3
rrjjzz <77rednaz77@gmail.com>
2026-05-01 16:47:28 -0700

    Add files via upload

---
+++ may_first_practice.R
@@ -0,0 +1,12 @@
+## Code to compute solution to quadratic equation of the form ax^2 + bx + x
+## Define variables
+a <- 4
+b <- 3
+c <- -2
+
+## Now compute solutions
+solution_1 <- (-b + sqrt(b^2 - 4*a*c)) / ( 2*a )
+solution_2 <- (-b - sqrt(b^2 - 4*a*c)) / ( 2*a )
+
+log(exp(1))
+log(8,2)
