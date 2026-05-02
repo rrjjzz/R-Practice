c8e4af366391780c60705937b14552668d8f8fa1
rrjjzz <77rednaz77@gmail.com>
2026-05-01 17:35:40 -0700

    Add files via upload

---
+++ may_first_practice_2 .R
@@ -0,0 +1,25 @@
+a <- 2
+class(a)
+class(ls)
+library(dslabs)
+data("murders")
+class(murders)
+str(murders)
+head(murders)
+murders$population
+names(murders)
+murders$region
+pop <- murders$population
+length(pop)
+class(pop)
+a <- 1
+a
+"a"
+class(murders$population)
+
+z <- 3 ==2
+z
+class(z)
+
+class(murders$region)
+levels(murders$region)
